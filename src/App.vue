e3a434862491e318c4813c516e18a98fbda801d4
Jake Mattson <JakeJMattson@gmail.com>
2020-01-05 23:51:35 -0600

    Test GitLab component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,28 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <GitLabProject name ="WarmBot"
+                   description="A discord bot for managing reports."
+                   repository="https://gitlab.com/Aberrantfox/WarmBot"/>
+
+    <ProjectCard>
+
+    </ProjectCard>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import GitLabProject from "./components/GitLabProject";
+import ProjectCard from "@/components/ProjectCard";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ProjectCard,
+    HelloWorld,
+    GitLabProject
   }
 }
 </script>
