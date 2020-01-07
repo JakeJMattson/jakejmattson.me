7cdec635813da5e0218914230d4966288eab690c
Jake Mattson <JakeJMattson@gmail.com>
2020-01-07 13:08:12 -0600

    Create NavigationBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import NavigationBar from "./components/NavigationBar"
 
 function App() {
   return (
-      <div>
-        Hello World
-      </div>
+      <NavigationBar/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
