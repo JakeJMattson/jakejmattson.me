0c6dc46feae9b5c1ff3fb4d6ee6e1a5261e7db90
Jake Mattson <JakeJMattson@gmail.com>
2020-01-07 23:15:36 -0600

    Create ProjectCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from "./components/NavigationBar"
+import ProjectCard from "./components/ProjectCard";
 
 function App() {
   return (
-      <NavigationBar/>
+      <body>
+        <NavigationBar/>
+        <ProjectCard
+            title = "Demo Project"
+            description = "Description of project"
+            imageLink = ""
+            pageLink = ""
+            repoLink = ""
+            gitHost = "GitHub"
+        />
+      </body>
   );
 }
 
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ProjectCard(props) {
+    return (
+        <div className="col-12 col-md-6 col-lg-4 pt-5 pb-3">
+            <div className="card mx-auto" style={{ maxWidth: 400 }}>
+                <div className="mb-0" style={{ height: 290, overflow: 'hidden' }}>
+                    <img alt="Demo" className="img-fluid rounded-top" src={ props.imageLink }
+                         style={{ verticalAlign: "middle", minHeight: "100%" }}>
+                    </img>
+                </div>
+                <div className="card-body" style={{ textAlign:"center" }}>
+                    <h5 className="card-title">{ props.title }</h5>
+                    <p className="card-text">{ props.description }</p>
+                    <div className="row">
+                        <div className="col-6">
+                            <a className="btn btn-outline-secondary" color={ "#00bfff" }
+                               href={ props.pageLink }>View Project</a>
+                        </div>
+                        <div className="col-6">
+                            <a className="btn btn-outline-secondary" color={ "#00bfff" }
+                               href={ props.repoLink }>
+                                <FontAwesomeIcon icon={['fab', props.gitHost.toLowerCase()]}/> { props.gitHost }
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCard
