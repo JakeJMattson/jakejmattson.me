f22413f24a78266676c4db2148e25b93f4ce38a1
Jake Mattson <JakeJMattson@gmail.com>
2020-01-13 00:52:26 -0600

    Populate project card text

--- src/components/pages/ProjectsPage.js
+++ src/components/pages/ProjectsPage.js
@@ -7,23 +7,81 @@ export default function ProjectPage() {
             <span className="navbar-brand" style={{ fontSize: "xx-large" }}>Personal Projects</span>
             <div className="row">
                 <ProjectCard
-                    title="Demo Project"
-                    description="Description of project"
+                    title="TouchControl"
+                    description="Control at your fingertips."
                     imageLink=""
                     pageLink=""
-                    repoLink=""
+                    repoLink="https://github.com/JakeJMattson/TouchControl/"
+                    gitHost="GitHub"
+                />
+                <ProjectCard
+                    title="EmbedBot"
+                    description="A Discord bot for managing embeds."
+                    imageLink=""
+                    pageLink=""
+                    repoLink="https://github.com/JakeJMattson/EmbedBot/"
+                    gitHost="GitHub"
+                />
+                <ProjectCard
+                    title="PixelDetails"
+                    description="Mouse location based pixel details."
+                    imageLink=""
+                    pageLink=""
+                    repoLink="https://github.com/JakeJMattson/PixelDetails/"
+                    gitHost="GitHub"
+                />
+                <ProjectCard
+                    title="AnyToImage"
+                    description="Convert files to images."
+                    imageLink=""
+                    pageLink=""
+                    repoLink="https://github.com/JakeJMattson/AnyToImage/"
+                    gitHost="GitHub"
+                />
+                <ProjectCard
+                    title="SpamBot"
+                    description="Multiplying automated browsers."
+                    imageLink=""
+                    pageLink=""
+                    repoLink="https://github.com/JakeJMattson/SpamBot/"
+                    gitHost="GitHub"
+                />
+                <ProjectCard
+                    title="FacialRecognition"
+                    description="Facial detection and recognition."
+                    imageLink=""
+                    pageLink=""
+                    repoLink="https://github.com/JakeJMattson/FacialRecognition/"
                     gitHost="GitHub"
                 />
             </div>
             <span className="navbar-brand" style={{ fontSize: "xx-large" }}>Contributed Projects</span>
-            <ProjectCard
-                title="Demo Project"
-                description="Description of project"
-                imageLink=""
-                pageLink=""
-                repoLink=""
-                gitHost="GitLab"
-            />
+            <div className="row">
+                <ProjectCard
+                    title="KUtils"
+                    description="A Kotlin Discord API."
+                    imageLink=""
+                    pageLink=""
+                    repoLink=""
+                    gitHost="GitLab"
+                />
+                <ProjectCard
+                    title="WarmBot"
+                    description="A report management discord bot."
+                    imageLink=""
+                    pageLink=""
+                    repoLink=""
+                    gitHost="GitLab"
+                />
+                <ProjectCard
+                    title="HotBot"
+                    description="A programming server discord bot."
+                    imageLink=""
+                    pageLink=""
+                    repoLink=""
+                    gitHost="GitLab"
+                />
+            </div>
         </div>
     );
 }
