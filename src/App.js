5576443d6835e5c63042de46d07d778ce837e215
Jake Mattson <JakeJMattson@gmail.com>
2020-01-14 22:54:25 -0600

    Add darkmodejs (broken)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import darkmodejs from "@assortment/darkmodejs"
+import { ThemeProvider } from "emotion-theming"
+import { Global } from "@emotion/core"
+import globalStyles from "./globalStyles"
+import themes from "./themes"
+
 import ProjectPage from "./components/pages/ProjectsPage";
 import HomePage from "./components/pages/HomePage";
-import { Navbar, Nav } from "react-bootstrap";
 
-export default function App() {
+const IndexPage = () => {
+    const [theme, setTheme] = useState("dark");
+
+    const onChange = (activeTheme, themes) => {
+        if (activeTheme === themes.light) {
+            setTheme(themes.light);
+            console.log(`Theme: ${themes.light}`);
+        } else {
+            setTheme(themes.dark);
+            console.log(`Theme: ${themes.dark}`);
+        }
+    };
+
+    useEffect(() => {
+        const dmjs = darkmodejs({ onChange });
+
+        return () => {
+            dmjs.removeListeners();
+        }
+    }, []);
+
     return (
-        <Router>
-            <div>
-                <Navbar bg="primary" variant="dark">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="projects">Projects</Nav.Link>
-                    </Nav>
-                </Navbar>
-                <Switch>
-                    <Route path="/projects">
-                        <ProjectPage/>
-                    </Route>
-                    <Route path="/">
-                        <HomePage/>
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <ThemeProvider theme={themes[theme]}>
+            <Global styles={globalStyles}/>
+            <Router>
+                <div>
+                    <Navbar bg="secondary" variant="dark">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="projects">Projects</Nav.Link>
+                        </Nav>
+                    </Navbar>
+                    <Switch>
+                        <Route path="/projects">
+                            <ProjectPage/>
+                        </Route>
+                        <Route path="/">
+                            <HomePage/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </ThemeProvider>
     );
-}
+};
+
+export default IndexPage
---
+++ src/themes.js
@@ -0,0 +1,18 @@
+export default {
+    light: {
+        background: {
+            color: "#FFF"
+        },
+        text: {
+            color: "#000"
+        }
+    },
+    dark: {
+        background: {
+            color: "#000"
+        },
+        text: {
+            color: "#FFF"
+        }
+    },
+}
