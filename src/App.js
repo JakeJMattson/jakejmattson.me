eaa2aabf2217789abd449dbd675b6d56ac82535c
Jake Mattson <JakeJMattson@gmail.com>
2020-01-15 13:19:37 -0600

    Fix theme switching

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 import darkmodejs from "@assortment/darkmodejs"
-import { ThemeProvider } from "emotion-theming"
-import { Global } from "@emotion/core"
-import globalStyles from "./globalStyles"
-import themes from "./themes"
 
 import ProjectPage from "./components/pages/ProjectsPage";
 import HomePage from "./components/pages/HomePage";
@@ -13,13 +9,13 @@ import HomePage from "./components/pages/HomePage";
 const IndexPage = () => {
     const [theme, setTheme] = useState("dark");
 
-    const onChange = (activeTheme, themes) => {
-        if (activeTheme === themes.light) {
-            setTheme(themes.light);
-            console.log(`Theme: ${themes.light}`);
+    const onChange = (activeTheme) => {
+        if (activeTheme === "light") {
+            setTheme("light");
+            console.log(`Theme: light`);
         } else {
-            setTheme(themes.dark);
-            console.log(`Theme: ${themes.dark}`);
+            setTheme("dark");
+            console.log(`Theme: dark`);
         }
     };
 
@@ -32,27 +28,24 @@ const IndexPage = () => {
     }, []);
 
     return (
-        <ThemeProvider theme={themes[theme]}>
-            <Global styles={globalStyles}/>
-            <Router>
-                <div>
-                    <Navbar bg="secondary" variant="dark">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="projects">Projects</Nav.Link>
-                        </Nav>
-                    </Navbar>
-                    <Switch>
-                        <Route path="/projects">
-                            <ProjectPage/>
-                        </Route>
-                        <Route path="/">
-                            <HomePage/>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-        </ThemeProvider>
+        <Router>
+            <div className={theme}>
+                <Navbar bg="secondary" variant="dark">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="projects">projects</Nav.Link>
+                    </Nav>
+                </Navbar>
+                <Switch>
+                    <Route path="/projects">
+                        <ProjectPage/>
+                    </Route>
+                    <Route path="/">
+                        <HomePage/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 };
 
